98a71c4139b371c14b4899eb61bce6045c8c8f3c
Joe <joe@wsl.home>
2023-08-08 15:57:42 +0200

    - following along with first example

--- app_01/src/firmware.c
+++ app_01/src/firmware.c
@@ -1,7 +1,34 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 
+#define LED_PIN  GPIO8
+#define LED_PORT GPIOA
+
+static void this_rcc_setup(void) {
+  rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_3V3_84MHZ]);
+}
+
+static void this_gpio_setup(void) {
+  rcc_periph_clock_enable(RCC_GPIOA);
+  gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_PIN);
+}
+
+static void delay_cycles(uint32_t cycles) {
+  volatile uint32_t i;
+  for (i = 0; i < cycles; i++) {
+    __asm__("nop");
+  }
+}
+
 int main(void) {
+
+  this_rcc_setup();
+  this_gpio_setup();
+
+  while (1) {
+    gpio_toggle(GPIOA, LED_PIN);
+    delay_cycles(8400000 / 4);
+  }
 
   return 0;
 }
